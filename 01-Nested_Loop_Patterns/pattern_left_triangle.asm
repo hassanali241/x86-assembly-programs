a9db8dcf6109f0723f9d82286a9a6348af0caadb
Hassan Ali <akhundhassan84@gmail.com>
2025-07-24 02:34:18 +0500

    Create pattern_left_triangle.asm

---
+++ 01-Nested_Loop_Patterns/pattern_left_triangle.asm
@@ -0,0 +1,40 @@
+.model small
+.stack 100h
+.data
+	num db '*'
+	counter db 1
+	newline db 13,10,'$'
+.code
+main proc
+
+	mov ax,@data
+	mov ds,ax
+
+	mov cx, 5
+outerloop:
+	push cx
+	
+	mov cl, counter
+
+innerloop:
+	
+	mov ah, 02h
+	mov dl, num
+	int 21h
+	
+	loop innerloop
+	
+	mov ah, 09h
+	lea dx, newline
+	int 21h
+
+	inc counter
+	pop cx
+
+	loop outerloop
+	
+	mov ah, 4ch
+	int 21h
+
+main endp
+end main
