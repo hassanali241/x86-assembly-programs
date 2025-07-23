63bbd93d8fccc7609a30eb76bf9337fe4febea14
Hassan Ali <akhundhassan84@gmail.com>
2025-07-24 02:46:42 +0500

    Create count_uppercase_chars.asm

---
+++ 3-StringOps_Operators_&_Stack/count_uppercase_chars.asm
@@ -0,0 +1,51 @@
+.model small
+.stack 100h
+.data
+	text1 db 'Enter sequence of character: $'
+	text2 db 'Number of capital characters: $'
+	count db 0
+.code
+
+main proc
+
+	mov ax, @data
+	mov ds, ax
+
+	mov ah, 09h
+	lea dx, text1
+	int 21h
+
+while_start:
+	mov ah, 01h
+	int 21h
+
+	cmp al, 0dh
+	je end_prog
+
+	cmp al, 'A'
+	jl while_start
+
+	cmp al, 'Z'
+	jg while_start
+
+	inc count
+	jmp while_start
+
+end_prog:
+
+	mov ah, 09h
+	lea dx, text2
+	int 21h
+
+	mov al, count
+	add al, '0'
+	mov dl, al
+	mov ah, 02h
+	int 21h
+
+ mov ah, 4ch
+ int 21h
+
+main endp
+end main
+	
