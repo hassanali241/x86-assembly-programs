bec93e40d16ed776c7589a65bd3a4bec0e8b13ae
Hassan Ali <akhundhassan84@gmail.com>
2025-07-24 02:54:32 +0500

    Create pattern_proc_2.asm

---
+++ 4-Procedures_&_Modularization/pattern_proc_2.asm
@@ -0,0 +1,55 @@
+.model small
+.stack 100h
+.data
+	count db '1'
+	space db 13,10, '$'
+	c db 1
+.code
+main proc 
+	mov ax, @data
+	mov ds, ax
+	
+	call myproc
+	
+	mov ah,4ch
+	int 21h
+
+main endp
+
+myproc proc
+	
+		mov cx, 5
+		
+outer_loop:
+	
+	push cx
+	mov cl, c
+
+inner_loop:
+
+	mov ah, 02h
+	mov dl, count
+	int 21h
+ 
+	loop inner_loop
+
+	inc count
+	
+	mov ah, 09h
+	lea dx, space
+	int 21h
+	
+	inc c
+
+	pop cx
+	
+	loop outer_loop
+	
+ret
+myproc endp
+	
+end main
+
+	
+	
+		
